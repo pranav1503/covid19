b378b04eff39268ac13a0bf3dfa14b524c5705e3
Pranav S <psmallrounder@gmail.com>
2020-04-14 21:45:46 +0530

    14-04-2020 #4

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
   $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "covid";
+  $username = "u960245160_root";
+  $password = "webdeveloper1510";
+  $dbname = "u960245160_covid";
 
   $conn = new mysqli($servername, $username, $password, $dbname);
   if ($conn->connect_error) {
